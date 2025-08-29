55ba3a2b1fd8106a22703144246092b61aea742a
吴子豪 <vortez.wohl@gmail.com>
2025-08-29 14:31:56 +0800

    feat: server 启动默认值约定

--- nioflux/server/server.py
+++ nioflux/server/server.py
@@ -1,16 +1,25 @@
 import asyncio
 import logging
+from typing_extensions import Any
 
 from nioflux.pipeline.stage import PipelineStage
 from nioflux.pipeline.pipeline import Pipeline
 from nioflux.util.readsuntil import readsuntil
 
+DEFAULT_HOST = '0.0.0.0'
+DEFAULT_PORT = 23860
+DEFAULT_TIMEOUT = 8.0
+DEFAULT_BUFFER_SIZE = 65536
+DEFAULT_EOT = b'<|eot|>'
+
 logger = logging.getLogger('nioflux.server')
 
 
 class Server:
-    def __init__(self, host: str, port: int, timeout: float, buffer_size: int,
-                 eot: bytes, pipeline: list[PipelineStage], extra: str):
+    def __init__(self, pipeline: list[PipelineStage],
+                 host: str = DEFAULT_HOST, port: int = DEFAULT_PORT,
+                 timeout: float = DEFAULT_TIMEOUT, buffer_size: int = DEFAULT_BUFFER_SIZE,
+                 eot: bytes = DEFAULT_EOT, extra: Any | None = None):
         assert len(pipeline) > 0, 'pipeline is empty'
         self._host = host
         self._port = port
@@ -40,13 +49,13 @@ class Server:
     def eot(self) -> bytes:
         return self._eot
 
-    async def channel_handler(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    async def _channel_handler(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         _peer_host, _peer_port = writer.get_extra_info('peername')
         logger.debug(f'Receiving data from {_peer_host}:{_peer_port}')
         try:
             data = await readsuntil(reader=reader, buffer_size=self._buffer_size,
                                     until=self._eot, timeout=self._timeout)
-            _, self._extra, _ = await Pipeline(queue=self.pipeline, data=data, extra=self._extra,
+            _, self._extra, _ = await Pipeline(queue=self._pipeline, data=data, extra=self._extra,
                                                io_ctx=(reader, writer), eot=self._eot).launch()
         except TimeoutError:
             logger.warning(f'Read timeout on channel {_peer_host}:{_peer_port}')
@@ -54,8 +63,9 @@ class Server:
             writer.close()
             await writer.wait_closed()
 
-    async def run(self, buffer_size: int = 65536):
-        server = await asyncio.start_server(client_connected_cb=self.channel_handler,
-                                            host=self.host, port=self.port, limit=buffer_size)
+    async def run(self):
+        server = await asyncio.start_server(client_connected_cb=self._channel_handler,
+                                            host=self._host, port=self._port,
+                                            limit=self._buffer_size)
         async with server:
             await server.serve_forever()
