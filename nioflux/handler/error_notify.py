330f5ea134d3528204abb783dce372572b257671
吴子豪 <vortezwohl@proton.me>
2025-08-30 03:05:48 +0800

    fix

--- nioflux/handler/error_notify.py
+++ nioflux/handler/error_notify.py
@@ -15,10 +15,10 @@ class ErrorNotify(PipelineStage):
     @override
     async def __call__(self, data: Any, extra: Any, err: list[Exception], fire: bool,
                        io_ctx: tuple[asyncio.StreamReader, asyncio.StreamWriter] | None) -> tuple[Any, Any, list[Exception], bool]:
-        reader, _ = io_ctx
-        peer = reader.get_extra_info('peername')
+        _, writer = io_ctx
+        _peer_host, _peer_port = writer.get_extra_info('peername')
         if len(err) > 0:
-            err_log = f'Error occurred on channel {peer[0]}:{peer[1]}'
+            err_log = f'Error occurred on channel {_peer_host}:{_peer_port}'
             for e in err:
                 err_log += f'\n- {type(e).__name__}: {e}'
             logger.error(err_log)
