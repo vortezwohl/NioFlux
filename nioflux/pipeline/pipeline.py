548a18c3cb4e77103ee4da2b8c64ae29242a4090
吴子豪 <vortez.wohl@gmail.com>
2025-08-29 12:59:23 +0800

    feat: 实现pipeline

--- nioflux/pipeline/pipeline.py
+++ nioflux/pipeline/pipeline.py
@@ -8,17 +8,17 @@ class Pipeline:
     def __init__(self, queue: list[PipelineStage], data: Any, extra: Any,
                  io_ctx: tuple[asyncio.StreamReader, asyncio.StreamWriter] | None, eof: bytes):
         self._queue = queue.copy()
-        self._io_ctx = io_ctx
         self._data = data
         self._extra = extra
-        self._fire = True
         self._err = []
+        self._fire = True
+        self._io_ctx = io_ctx
         self._eof = eof
 
     async def __call__(self) -> tuple[Any, Any, list[Exception]]:
         if not isinstance(self._queue, list):
             return self._data, self._extra, self._err
-        if len(self._queue):
+        if len(self._queue) > 0:
             for stage in self._queue:
                 self._data, self._extra, self._err, self._fire = await stage(data=self._data, extra=self._extra,
                                                                              err=self._err, fire=self._fire,
