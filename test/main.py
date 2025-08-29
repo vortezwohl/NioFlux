0e560a734a87ae3b35a61a2be01fc0bdeb0d35d9
吴子豪 <vortez.wohl@gmail.com>
2025-08-29 14:52:38 +0800

    update: 添加测试脚本

---
+++ test/main.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from nioflux import Server, StrDecode, StrEncode, PipelineStage
+
+
+class MyHandler(PipelineStage):
+    def __init__(self):
+        super().__init__(label='my_handler')
+
+    async def __call__(self, data, extra, err, fire, io_ctx, eot):
+        print('My data:', data)
+        return data, extra, err, fire
+
+
+async def main():
+    server = Server([StrDecode(), MyHandler(), StrEncode()])
+    await server.run()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
