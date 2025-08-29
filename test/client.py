835f0c417a2463c545f5fb064cd4af943eebe273
吴子豪 <vortez.wohl@gmail.com>
2025-08-29 15:22:25 +0800

    update: 新增测试脚本

--- test/client.py
+++ test/client.py
@@ -1,4 +1,5 @@
 from nioflux.util import tcp_send
 
 if __name__ == '__main__':
-    tcp_send(b'hello world', '127.0.0.1', 2386, wait=False)
+    for _ in range(10):
+        tcp_send(f'hello world {_}'.encode('utf-8'), '127.0.0.1', 2386, wait=False)
