559dba5f75b333ccbe09d2cc496a0738228bf967
吴子豪 <vortez.wohl@gmail.com>
2025-08-29 12:55:18 +0800

    feat: 实现pipeline

---
+++ nioflux/pipeline/pipeline.py
@@ -0,0 +1,41 @@
+import asyncio
+from typing_extensions import Any
+
+from nioflux.pipeline.stage import PipelineStage
+
+
+class Pipeline:
+    def __init__(
+            self,
+            queue: list[PipelineStage],
+            data: Any,
+            extra: Any,
+            io_ctx: tuple[asyncio.StreamReader, asyncio.StreamWriter] | None,
+            eof: bytes
+    ):
+        self._queue = queue.copy()
+        self._io_ctx = io_ctx
+        self._data = data
+        self._extra = extra
+        self._fire = True
+        self._err = []
+        self._eof = eof
+
+    async def __call__(
+            self
+    ) -> tuple[Any, Any, list[Exception]]:
+        if not isinstance(self._queue, list):
+            return self._data, self._extra, self._err
+        if len(self._queue):
+            for stage in self._queue:
+                self._data, self._extra, self._err, self._fire = await stage.do(
+                    data=self._data,
+                    extra=self._extra,
+                    err=self._err,
+                    fire=self._fire,
+                    io_ctx=self._io_ctx,
+                    eof=self._eof
+                )
+                if not self._fire:
+                    break
+        return self._data, self._extra, self._err
---
+++ nioflux/pipeline/stage.py
@@ -0,0 +1,25 @@
+import asyncio
+from abc import abstractmethod
+from typing_extensions import Any, List
+
+
+class PipelineStage:
+    def __init__(self, label: str):
+        self._label: str = label
+
+    @property
+    def label(self) -> str:
+        return self._label
+
+    @abstractmethod
+    async def __call__(
+            self,
+            data: Any,
+            extra: Any,
+            err: List[Exception],
+            fire: bool,
+            io_ctx: tuple[asyncio.StreamReader, asyncio.StreamWriter] | None,
+            eof: bytes
+    ) -> tuple[Any, Any, List[Exception], bool]:
+        # data, extra, err, fire
+        ...
