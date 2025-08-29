7dcb92c3dd8cfddd7c09e0ab2240f3e76975ae29
吴子豪 <vortez.wohl@gmail.com>
2025-08-29 15:25:36 +0800

    update

--- test/client.py
+++ test/client.py
@@ -1,5 +1,6 @@
+from nioflux.server.server import DEFAULT_EOT
 from nioflux.util import tcp_send
 
 if __name__ == '__main__':
-    for _ in range(10):
-        tcp_send(f'hello world {_}'.encode('utf-8'), '127.0.0.1', 2386, wait=False)
+    for _ in range(50):
+        tcp_send(f'hello world {_}'.encode('utf-8') + DEFAULT_EOT, '127.0.0.1', 2386, wait=False)
